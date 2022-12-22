3e90396f810bad65027c8fca32a539ca424e6f05
d1av <davi4alves@gmail.com>
2022-12-22 16:18:18 -0300

    feat: entities Users and Roles configured and database seeded

---
+++ event-city/src/main/java/io/davi/tdd2/config/AppConfig.java
@@ -0,0 +1,8 @@
+package io.davi.tdd2.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ event-city/src/main/java/io/davi/tdd2/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package io.davi.tdd2.repositories;
+
+import io.davi.tdd2.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
